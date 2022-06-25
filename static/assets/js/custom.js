2ba28ccbc0afa012c157f5a31fa54ccf8833f706
CMEONE <contact@kabirwearsato.ga>
2022-06-25 13:01:44 -0700

    Add authentication scripts

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -220,20 +220,20 @@
 
 	// Page loading animation
 	$(window).on('load', function() {
-		if($('.cover').length){
-			$('.cover').parallax({
-				imageSrc: $('.cover').data('image'),
-				zIndex: '1'
-			});
-		}
+		// if($('.cover').length){
+		// 	$('.cover').parallax({
+		// 		imageSrc: $('.cover').data('image'),
+		// 		zIndex: '1'
+		// 	});
+		// }
 
-		$("#preloader").animate({
-			'opacity': '0'
-		}, 600, function(){
-			setTimeout(function(){
-				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
-		});
+		// $("#preloader").animate({
+		// 	'opacity': '0'
+		// }, 600, function(){
+		// 	setTimeout(function(){
+		// 		$("#preloader").css("visibility", "hidden").fadeOut();
+		// 	}, 300);
+		// });
 	});
 
 	
