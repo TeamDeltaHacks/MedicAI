79d69062d36f6b7d78e0cca3d392c83267ae9ccd
CMEONE <contact@kabirwearsato.ga>
2022-06-25 12:11:43 -0700

    Add register and login pages

--- app.py
+++ app.py
@@ -25,3 +25,11 @@ def add():
 	else:
 		json = request.json
 		return '{"type":"success","response":"result"}'
+
+@app.route('/register', methods=['GET'])
+def register():
+	return render_template('register.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+	return render_template('login.html')
