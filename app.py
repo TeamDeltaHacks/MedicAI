2ecb47ede64f592a36ea8b6f88220326ed0e973a
CMEONE <contact@kabirwearsato.ga>
2022-06-25 19:05:54 -0700

    Parse image of report

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import os
-import io
-import base64
+import cv2
+import pytesseract
+import numpy as np
+import urllib
+import json as JSON
 
 app = Flask(__name__)
 
@@ -15,7 +17,7 @@ def view_records():
 	return render_template('records.html')
 
 @app.route('/records/<record>', methods=['GET'])
-def records():
+def records(record):
 	return render_template('record.html', record=record)
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -23,8 +25,32 @@ def add():
 	if(request.method == 'GET'):
 		return render_template('add.html')
 	else:
-		json = request.json
-		return '{"type":"success","response":"result"}'
+		try:
+			json = request.json
+			if("report" not in json or json["report"] == ""):
+				return JSON.dumps({
+					"type": "error",
+					"response": "Missing field: image."
+				})
+
+			req = urllib.request.urlopen(json["report"])
+			arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+			image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+			custom_config = r'--oem 3 --psm 6'
+			parsed = pytesseract.image_to_string(image, config=custom_config)
+
+			return JSON.dumps({
+				"type": "success",
+				"response": {
+					"parsed": parsed
+				}
+			})
+		except Exception as e:
+			print(e)
+			return JSON.dumps({
+				"type": "error",
+				"response": "Invalid request, please try again."
+			})
 
 @app.route('/register', methods=['GET'])
 def register():
